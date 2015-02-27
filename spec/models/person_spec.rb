f14f585c99a4ef0815432bc060dc9471b7927847
Jeffrey Gu <jeffwgu@gmail.com>
2015-02-27 11:02:08 -0700

    person tests passing

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+  let(:person) { Person.new(first_name: 'Alice', last_name: 'Smith') }
+
   it 'is invalid without first name' do
-    person = Person.new(first_name: nil)
+    person.first_name = nil
     expect(person).not_to be_valid
   end
 
   it 'is invalid without last name' do
-    person = Person.new(first_name: 'bob', last_name: nil)
+    person.last_name = nil
     expect(person).not_to be_valid
+  end
+
+  it 'is valid' do
+    expect(person).to be_valid
   end
 end
