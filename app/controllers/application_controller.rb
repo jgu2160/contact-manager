4d4363e487dcd0f3a9b7374ad6d075bc0c20fa6c
Jeffrey Gu <jeffwgu@gmail.com>
2015-03-01 19:13:11 -0700

    nil session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   def find_resource
     class_name = params[:controller].singularize
     klass = class_name.camelize.constantize
     self.instance_variable_set "@" + class_name, klass.find(params[:id])
+  end
+
+  def current_user
+    require 'byebug'; byebug
+    @current_user ||= User.find(session[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :companies
-
   resources :emails, except: [:index, :show]
-
   resources :phone_numbers, except: [:index, :show]
-
   resources :people
+  resource :sessions, :only => [:create]
+  #root to: 'companies#index'
+  root to: 'site#index'
+  match "/login" => redirect("/auth/twitter"), as: :login, via: [:get, :post]
+  get '/auth/:provider/callback' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
